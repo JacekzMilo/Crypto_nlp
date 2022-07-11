1258fdca9a0b927d2dae8a2224adfc6c62324e13
Jacek <61091672+JacekzMilo@users.noreply.github.com>
2022-07-11 11:00:27 +0200

    Descriptions translated, spider updated

--- Scraper/Scraper/spiders/zrozumiecbitc.py
+++ Scraper/Scraper/spiders/zrozumiecbitc.py
@@ -11,19 +11,20 @@ class ZrozumiecbitcSpider(scrapy.Spider):
     allowed_domains = ['www.zrozumiecbitcoina.pl']
     # URL(s) to scrape as mentioned in the "genspider" command
     # The scrapy spider, starts making  requests, to URLs mentioned here
-    start_urls = ['https://www.zrozumiecbitcoina.pl/2020/']
+    start_urls = ['https://www.zrozumiecbitcoina.pl']
 
     # Default callback method responsible for returning the scraped output and processing it.
     def parse(self, response):
         # XPath expression of all the Quote elements.
         # All quotes belong to CSS attribute class having value 'quote'
         # base_url = 'https://www.zrozumiecbitcoina.pl/2020'
-        zrozumiecbitc = response.xpath("//h1/a")
+        # zrozumiecbitc = response.xpath("//h1/a")
+        zrozumiecbitc = response.xpath("/html/body/div[2]/div/div/section[8]/div/div/div/div/div/div[1]/div/div/article[4]/div/div[3]/h3/a")
+
         # for bitc in zrozumiecbitc:
         title = zrozumiecbitc.xpath(".//text()").get()
         link = zrozumiecbitc.xpath(".//@href").get()
         # full_link=base_url+link
-
 
         yield response.follow(url=link, callback=self.parse_zrozumiecbitc, meta={'zrozumiecbitc_title': title, 'zrozumiecbitc_link': link})
 
@@ -32,13 +33,10 @@ class ZrozumiecbitcSpider(scrapy.Spider):
         item['article_name'] = response.request.meta['zrozumiecbitc_title']
         item['article_link'] =response.request.meta['zrozumiecbitc_link']
 
-        articles = response.xpath('(//*[@id="content-2403"])/div')
+        articles = response.xpath('(//*[@id="content-3476"]/div[2])')
         # for article in articles:
         item['article_text'] = articles.xpath(".//p/text()").getall()
 
-        # item['article_text_1'] = list(item['article_text_1'])
-        # item['article_text_1'] = [i.replace("\t", "").replace("\n", "") for i in item['article_name_1']]
-        # item['article_text_1'] = [i.replace("\u00A0", " ") for i in item['article_name_1']]
         yield item
         logging.info(response.url)
 
--- Scraper/main.py
+++ Scraper/main.py
@@ -7,47 +7,47 @@ from Crypto_nlp.Scraper.Scraper.spiders import zrozumiecbitc, bitcoin_spider, in
 import os
 
 
-# ###################### Below code runs scrapping module
-# settings_file_path = 'Scraper.settings' # The path seen from root, ie. from main.py
-# os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_file_path)
-# crawlers = [zrozumiecbitc.ZrozumiecbitcSpider, bitcoin_spider.BitcoinSpider,
-#             insights_glassnode_spider.InsightsGlassnodeSpider,
-#             coin_desk_spider.CoinDeskSpider, bitcoin_magazine_spider.BitcoinMagazineSpider
-#             ]
-#
-# # TODO: the_block_spider.TheBlockSpider i the_defiant_spider.TheDefiantSpider i blockworks_spider.BlockWorksSpider
-#
-# # TODO: coindeskspider has hardcoded div range and may not cover all article
-#
-# def start_sequentially(process: CrawlerProcess, crawlers: list):
-#     print('start crawler {}'.format(crawlers[0].__name__))
-#     deferred = process.crawl(crawlers[0])
-#     if len(crawlers) > 1:
-#         deferred.addCallback(lambda _: start_sequentially(process, crawlers[1:]))#
-#
-# process = CrawlerProcess(settings=get_project_settings())
-# start_sequentially(process, crawlers)
-# # settings=get_project_settings()
-# # process = CrawlerProcess(settings)
-# # process.crawl(zrozumiecbitc.ZrozumiecbitcSpider)
-# process.start()
-# ######################
-#
-#
-# ###################### Tu by sie przydało usprawnić. Gdy raz przejdzie przez customtextfunc i ponownie chce przejsc to wyskakuje blad.
-# # Below code runs customtextfunc that puts scrapped text into article table in BQ
-#
-# file = 'C:/Users/Jacklord/PycharmProjects/Crypto_nlp/Crypto_nlp/Scraper/Scraper/spiders/Scraped_data.json'
-# customtextfunc(file)
-# #######################
-#
-#
-# ####################### Below code runs article_translation function that translate article text into English and puts
-# # the data into article_translated table in BQ
-#
-# filename = 'C:/Users/Jacklord/PycharmProjects/Crypto_nlp/Crypto_nlp/Scraper/Scraper/spiders/Scraped_data.csv'
-# article_translation(filename)
-# #######################
+###################### Below code runs scrapping module
+settings_file_path = 'Scraper.settings' # The path seen from root, ie. from main.py
+os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_file_path)
+crawlers = [zrozumiecbitc.ZrozumiecbitcSpider, bitcoin_spider.BitcoinSpider,
+            insights_glassnode_spider.InsightsGlassnodeSpider,
+            coin_desk_spider.CoinDeskSpider, bitcoin_magazine_spider.BitcoinMagazineSpider
+            ]
+
+# TODO: the_block_spider.TheBlockSpider i the_defiant_spider.TheDefiantSpider i blockworks_spider.BlockWorksSpider
+
+# TODO: coindeskspider has hardcoded div range and may not cover all article
+
+def start_sequentially(process: CrawlerProcess, crawlers: list):
+    print('start crawler {}'.format(crawlers[0].__name__))
+    deferred = process.crawl(crawlers[0])
+    if len(crawlers) > 1:
+        deferred.addCallback(lambda _: start_sequentially(process, crawlers[1:]))#
+
+process = CrawlerProcess(settings=get_project_settings())
+start_sequentially(process, crawlers)
+# settings=get_project_settings()
+# process = CrawlerProcess(settings)
+# process.crawl(zrozumiecbitc.ZrozumiecbitcSpider)
+process.start()
+######################
+
+
+###################### Tu by sie przydało usprawnić. Gdy raz przejdzie przez customtextfunc i ponownie chce przejsc to wyskakuje blad.
+# Below code runs customtextfunc that puts scrapped text into article table in BQ
+
+file = 'C:/Users/Jacklord/PycharmProjects/Crypto_nlp/Crypto_nlp/Scraper/Scraper/spiders/Scraped_data.json'
+customtextfunc(file)
+#######################
+
+
+####################### Below code runs article_translation function that translate article text into English and puts
+# the data into article_translated table in BQ
+
+filename = 'C:/Users/Jacklord/PycharmProjects/Crypto_nlp/Crypto_nlp/Scraper/Scraper/spiders/Scraped_data.csv'
+article_translation(filename)
+#######################
 
 
 #######################the code below creates the sentence_polarity_hisogram_plot.csv and puts the sentence_polarity_distribution_plot into the tables on the BQ
